cb20cb6ecd96feb53651021294f2ad53cb08af9a
Dinatang <dm.tanguilav@uea.edu.ec>
2025-08-14 18:16:53 -0500

    subir

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Ruta principal
 @app.route('/')
 def index():
     return "Hola, mundo!"
 
+# Ruta con un parámetro dinámico
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Bienvenido, {Dina}!'
+    return f'Bienvenido, {nombre}!'
+
+# Ruta con dos parámetros dinámicos
+@app.route('/saludo/<nombre>/<int:edad>')
+def saludo(nombre, edad):
+    return f'Hola {nombre}, tienes {edad} años!'
 
 if __name__ == '__main__':
     app.run(debug=True)
