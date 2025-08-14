a86e83c21c56a052f19a7403f6de72f8fcb8a7f4
Dinatang <dm.tanguilav@uea.edu.ec>
2025-08-14 17:25:18 -0500

    cambio el nombre

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
+    return f'Bienvenido, {Dina}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
