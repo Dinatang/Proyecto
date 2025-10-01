59a809d194280f668e242175a924aa578daf0c70
Dinatang <dm.tanguilav@uea.edu.ec>
2025-09-30 19:00:44 -0500

    semana 16 actualización

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ class Producto(db.Model):
 class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(120), nullable=False)
-    descripcion = db.Column(db.String(200))  # 👈 añadido
     correo = db.Column(db.String(120), unique=True, nullable=False)
     telefono = db.Column(db.String(20))
     direccion = db.Column(db.String(200))
